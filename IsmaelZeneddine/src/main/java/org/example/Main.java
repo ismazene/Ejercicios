184c3f963a23db3f3f9f8c247680c04cfa5be152
A604\1DAW <edgarfer2205@gmail.com>
2024-12-03 17:20:18 +0100

    no message

--- IsmaelZeneddine/src/main/java/org/example/Main.java
+++ IsmaelZeneddine/src/main/java/org/example/Main.java
@@ -3,13 +3,35 @@ import org.example.TEMA1.*;
 import org.example.TEMA2.*;
 import org.example.TEMA3.*;
 
-
-
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        PRACTICAST3 practicast3 = new PRACTICAST3();
-        practicast3.practica4();
+        Scanner teclado = new Scanner(System.in);
+        Random aleatorio = new Random();
+
+        System.out.println("Cara Cruz????");
+        String opcion = teclado.next();
+
+        //LLAMAR AL SORTEO
+        String resultado = sorteo();
+
+            if (resultado.equals(opcion)) {
+                System.out.println("Has ganado. Ha salido: " + resultado);
+            } else {
+                System.out.println("Has perdido. Ha salido: " + resultado);
+            }
+        }
+    public static void comprobarOpcion (String resultado, String opcion){
+
+    }
+
+    public static String sorteo() {
+        Random aleatorio = new Random();
+        String opciones[] = {"Cara", "Cruz"};
+
+        String resultado = opciones[aleatorio.nextInt(2)];
+        return resultado;
     }
 }
-
