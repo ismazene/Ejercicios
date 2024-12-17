6c3276981f7e51e2ab290147cc07015d4607841d
DESKTOP-2TUSL7F\Usuario <ismaelzeneddine@gmail.com>
2024-12-17 14:48:38 +0100

    ++

--- IsmaelZeneddine/src/main/java/org/example/Verdugo.java
+++ IsmaelZeneddine/src/main/java/org/example/Verdugo.java
@@ -1,83 +1,81 @@
 package org.example;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Proyecto del ProgramaMe: Juego del Ahorcado/Vedugo
- * @author Ismael Zeneddine Ouahdi
+ * Proyecto del ProgramaMe: Juego del Ahorcado/Verdugo
+ * @author
  * @version 1.1 (12/12/2024)
  */
 
 public class Verdugo {
+    Scanner teclado = new Scanner(System.in);
 
-    /**
-     * Este es el metodo proncipal en el que entran todos los datos de los demas metodos
-     */
-    public static void principal() {
-        Scanner teclado = new Scanner(System.in);
+    public void principal() {
         String resultado;
 
-        // Repite el proceso hasta que el usuario ingrese "."
         while (true) {
             String respuesta = teclado.next().trim();
             if (respuesta.equals(".")) {
                 break;
             }
 
-            String[] letrasUsuario1 = Usuario1(respuesta);
-            String[] letrasUsuario2 = Usuario2Adivinador();
-            resultado = Comprobacion(letrasUsuario1, letrasUsuario2);
+            String[] letrasUsuario1 = usuario1(respuesta);
+            String[] letrasUsuario2 = usuario2Adivinador();
+            resultado = comprobacion(letrasUsuario1, letrasUsuario2);
             System.out.println(resultado);
-
         }
     }
 
-    /**
-     *
-     * @return
-     */
-    public static String[] Usuario1(String respuesta) {
-        //Scanner teclado = new Scanner(System.in);
-        //String palabra = teclado.next().trim().toLowerCase();
+    public static String[] usuario1(String respuesta) {
         String palabra = respuesta.trim().toLowerCase();
-
-        if (palabra.length() > 100) {
-            System.err.println("Error: No puedes poner más de 100 letras.");
-        }
-
         String[] letrasUsuario1 = palabra.split("");
         return letrasUsuario1;
     }
 
-    public static String[] Usuario2Adivinador() {
+    public static String[] usuario2Adivinador() {
         Scanner teclado = new Scanner(System.in);
         String letras = teclado.next().trim().toLowerCase();
         String[] letrasUsuario2 = letras.split("");
         return letrasUsuario2;
     }
 
-    public static String Comprobacion(String[] letrasUsuario1, String[] letrasUsuario2) {
-        int aciertos = 0;
-        String letras_acertadas = "";
+    public static String comprobacion(String[] letrasUsuario1, String[] letrasUsuario2) {
         int falladas = 0;
+        String letrasAcertadas = "";
+        String letrasFalladas = "";
 
-        for (int i = 0; i < letrasUsuario1.length; i++) {
-            for (int j = 0; j < letrasUsuario2.length; j++) {
-                if (letrasUsuario1[i].equals(letrasUsuario2[j]) && !letras_acertadas.contains(letrasUsuario1[i])) {
-                    aciertos++;
-                    letras_acertadas += letrasUsuario1[i];
+        for (int i = 0; i < letrasUsuario2.length; i++) {
+            String letra = letrasUsuario2[i];
+
+            if (letrasAcertadas.contains(letra) || letrasFalladas.contains(letra)) {
+                continue;
+            }
+
+            boolean encontrada = false;
+            for (int j = 0; j < letrasUsuario1.length; j++) {
+                if (letra.equals(letrasUsuario1[j])) {
+                    encontrada = true;
+                    break;
                 }
             }
-        }
 
-        System.out.println(aciertos + " " + falladas + " " + letras_acertadas);
+            if (encontrada) {
+                letrasAcertadas += letra;
+            } else {
+                letrasFalladas += letra;
+                falladas++;
+            }
 
-        if ((letrasUsuario2.length - aciertos) >=7){
-            return "AHORCADO";
-        } else if (letras_acertadas.equals()) {
-            return "SALVADO";
-        } else{
-            return "COLGANDO";
+            if (falladas >= 7) {
+                return "AHORCADO";
+            }
+
+            if (letrasAcertadas.length() == letrasUsuario1.length) {
+                return "SALVADO";
+            } else {
+                return "COLGANDO";
+            }
         }
+        return "";
     }
 }
