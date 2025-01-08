69aa4b88fb7545699fbb774cced73e65ea3d6348
A604\1DAW <edgarfer2205@gmail.com>
2025-01-08 21:15:35 +0100

    ++

--- IsmaelZeneddine/src/main/java/org/example/Main.java
+++ IsmaelZeneddine/src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import org.example.TEMA4.Ejercicios;
+
 public class Main {
     public static void main(String[] args) {
+        Ejercicios.funcion();
+        
     }
 }
--- IsmaelZeneddine/src/main/java/org/example/TEMA4/Ejercicios.java
+++ IsmaelZeneddine/src/main/java/org/example/TEMA4/Ejercicios.java
@@ -1,4 +1,19 @@
 package org.example.TEMA4;
+import java.util.Scanner;
 
 public class Ejercicios {
-}
+        public static void funcion(){
+            Scanner teclado = new Scanner(System.in);
+            int nBase = 1;
+            System.out.println("Dame un numero");
+            int num = teclado.nextInt();
+            if (num > nBase){
+
+                funcion();
+
+            }else{
+                //código
+                return;
+            }
+        }
+    }
